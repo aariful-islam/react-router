988e5662c9068d5f165939aa1313d82dfb54a3da
arif99050 <arifulislam99050@gmail.com>
2022-04-01 23:04:07 +0600

    use customLink

--- src/Componetns/Header/Header.js
+++ src/Componetns/Header/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+import CustomLink from '../CustomLink/CustomLink';
 import './Header.css'
 
 const Header = () => {
     return (
+        
         <div className='header'>
-            <Link to="/">Home</Link>
-            <Link to="/friends">Friends</Link>
-            <Link to="/about">About</Link>
+            
+            <CustomLink to="/">Home</CustomLink>
+            <CustomLink to="/friends">Friends</CustomLink>
+            <CustomLink to="/about">About</CustomLink>
             
             
         </div>
